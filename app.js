5048b28b3bd09c45d8712057ab6369b963a94651
nhatn1507 <nvn@mit.edu>
2017-11-12 16:01:16 -0500

    zz

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
-	document.body.onclick = () => $('body').css('background-image', 'url(rose.gif)');
-	$('body').awesomeCursor('heart', {color: '#5c6fd8', size: '50px'});
-});
+$('body').awesomeCursor('heart', {color: '#5c6fd8', size: '50px'});
+	$('body').on('click touch', function() {
+		$('body').css('background-image', 'url(rose.gif)');
+		
+	});
+
+})
