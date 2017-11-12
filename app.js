29171eb37cb3ccb82902eeddb77af667460004eb
nhatn1507 <nvn@mit.edu>
2017-11-12 15:20:40 -0500

    love

---
+++ app.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+	document.body.onclick = () => $('body').css('background-image', 'url(rose.gif)');
+	$('body').awesomeCursor('heart', {color: '#5c6fd8', size: '50px'});
+}
